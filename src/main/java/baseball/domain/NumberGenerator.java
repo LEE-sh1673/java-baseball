7155c4c8574a09726c51f915e619da87c42dc310
LEE-sh1673 <lsh901673@gmail.com>
2022-11-09 19:54:41 +0900

    feat(baseball): 중복되지 않은 임의의 숫자들을 생성하는 기능 추가

--- src/main/java/baseball/domain/NumberGenerator.java
+++ src/main/java/baseball/domain/NumberGenerator.java
@@ -1,9 +1,35 @@
 package baseball.domain;
 
 import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
 
 public class NumberGenerator {
+
+    private static final int MIN_NUMBER = 1;
+
+    private static final int MAX_NUMBER = 9;
+
+    private static final int MAX_NUMBERS_SIZE = 3;
+
+    public static List<Integer> createNonDuplicateNumbers() {
+        List<Integer> numbers = new ArrayList<>();
+
+        while (numbers.size() < MAX_NUMBERS_SIZE) {
+            addNonDuplicateNumberTo(numbers);
+        }
+        return numbers;
+    }
+
+    private static void addNonDuplicateNumberTo(final List<Integer> numbers) {
+        int number = createRandomNumber();
+
+        if (!numbers.contains(number)) {
+            numbers.add(number);
+        }
+    }
+
     public static int createRandomNumber() {
-        return Randoms.pickNumberInRange(1, 9);
+        return Randoms.pickNumberInRange(MIN_NUMBER, MAX_NUMBER);
     }
 }
--- src/test/java/baseball/domain/NumberGeneratorTest.java
+++ src/test/java/baseball/domain/NumberGeneratorTest.java
@@ -2,7 +2,11 @@ package baseball.domain;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Test;
 
 class NumberGeneratorTest {
@@ -14,4 +18,13 @@ class NumberGeneratorTest {
         assertThat(number).isBetween(1, 9);
     }
 
+    @DisplayName("중복되지 않는 임의의 3자리 숫자 생성 테스트")
+    @RepeatedTest(10)
+    void createNonDuplicateNumbers_ShouldReturnNonDuplicateNumbers() {
+        List<Integer> numbers
+            = NumberGenerator.createNonDuplicateNumbers();
+
+        Set<Integer> nonDuplicateNumbers = new HashSet<>(numbers);
+        assertThat(nonDuplicateNumbers.size()).isEqualTo(numbers.size());
+    }
 }
