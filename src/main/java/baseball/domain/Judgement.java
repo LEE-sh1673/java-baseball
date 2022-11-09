972eaf852a69fc30a57b75a419c0ec0c700faa57
LEE-sh1673 <lsh901673@gmail.com>
2022-11-09 20:38:01 +0900

    feat(baseball): 특정 자리에 특정 숫자가 있는지 구하는 기능 추가

--- src/main/java/baseball/domain/Judgement.java
+++ src/main/java/baseball/domain/Judgement.java
@@ -4,8 +4,13 @@ import java.util.List;
 
 public class Judgement {
 
+    public static boolean hasPlace(final List<Integer> numbers,
+        final int number, final int index) {
+        return numbers.indexOf(number) == index;
+    }
+
     public int correctCount(final List<Integer> computer, final List<Integer> player) {
-        return (int)player.stream()
+        return (int) player.stream()
             .filter(computer::contains)
             .count();
     }
--- src/test/java/baseball/domain/JudgementTest.java
+++ src/test/java/baseball/domain/JudgementTest.java
@@ -8,18 +8,21 @@ import java.util.stream.Stream;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.CsvSource;
 import org.junit.jupiter.params.provider.MethodSource;
 
 public class JudgementTest {
 
     static Judgement judgement = new Judgement();
 
+    static List<Integer> computer = List.of(1, 2, 3);
+
     @DisplayName("몇 개의 숫자가 같은지 구하는 기능 테스트")
     @ParameterizedTest
     @MethodSource("generateData")
     void correctCount_ShouldGetSameValueCount(List<Integer> player, Integer expected) {
         assertThat(
-            judgement.correctCount(List.of(1, 2, 3), player)
+            judgement.correctCount(computer, player)
         ).isEqualTo(expected);
     }
 
@@ -30,4 +33,16 @@ public class JudgementTest {
             Arguments.of(Arrays.asList(4, 1, 7), 1)
         );
     }
+
+    @DisplayName("특정 자리에 특정 숫자가 있는지 구하는 기능 테스트")
+    @ParameterizedTest
+    @CsvSource({"2,1,true", "1,1,false", "3,2,true"})
+    void hasPlace_ShouldReturnTrueNumberInPlace(Integer number, Integer index,
+        boolean expected) {
+
+        assertThat(
+            Judgement.hasPlace(computer, number, index)
+        ).isEqualTo(expected);
+    }
+
 }
