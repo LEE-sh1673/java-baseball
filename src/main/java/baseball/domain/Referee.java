05712df2c83b41000b30abb86cad7fb6c52606c8
LEE-sh1673 <lsh901673@gmail.com>
2022-11-09 22:01:29 +0900

    refactor(baseball): Referee에 생성자 추가 및 메서드 시그니처 정리

--- src/main/java/baseball/domain/Referee.java
+++ src/main/java/baseball/domain/Referee.java
@@ -5,21 +5,24 @@ import java.util.stream.IntStream;
 
 public class Referee {
 
+    private final Judgement judgement;
+
+    public Referee() {
+        this.judgement = new Judgement();
+    }
+
     public String compare(final List<Integer> computer, final List<Integer> player) {
-        Judgement judgement = new Judgement();
         int correctCount = judgement.correctCount(computer, player);
 
         if (correctCount == 0) {
             return "낫싱";
         }
-        int numberOfStrikes = getNumberOfStrikes(computer, player, judgement);
+        int numberOfStrikes = getNumberOfStrikes(computer, player);
         int numberOfBalls = correctCount - numberOfStrikes;
         return String.format("%d볼 %d스트라이크", numberOfBalls, numberOfStrikes);
     }
 
-    private static int getNumberOfStrikes(final List<Integer> computer,
-        final List<Integer> player, final Judgement judgement) {
-
+    private int getNumberOfStrikes(final List<Integer> computer, final List<Integer> player) {
         return (int)IntStream.range(0, computer.size())
             .filter(i -> judgement.hasPlace(player, computer.get(i), i))
             .count();
