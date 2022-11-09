a35103bd304e489a5a6679197cf91224fa899219
LEE-sh1673 <lsh901673@gmail.com>
2022-11-09 21:41:40 +0900

    refactor(controller): 생성자 추가 및 메서드 분리

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -9,15 +9,26 @@ import java.util.List;
 
 public class BaseballGameController {
 
+    private final Referee referee;
+
+    private String playResult;
+
+    public BaseballGameController() {
+        referee = new Referee();
+        playResult = "";
+    }
+
     public void start() {
-        Referee referee = new Referee();
+        processGame();
+        OutputView.displayGameEndMessage();
+    }
+
+    private void processGame() {
         List<Integer> answer = createNonDuplicateNumbers();
-        String playResult = "";
 
         while (!playResult.equals("0볼 3스트라이크")) {
             playResult = referee.compare(answer, InputView.askNumbers());
             OutputView.displayPlayResult(playResult);
         }
-        OutputView.displayGameEndMessage();
     }
 }
