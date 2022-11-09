20c9e8b63d44b677dbf40895c4953fe587dd706e
LEE-sh1673 <lsh901673@gmail.com>
2022-11-09 21:49:54 +0900

    feat(service): BaseballService 추가

--- src/main/java/baseball/controller/BaseballGameController.java
+++ src/main/java/baseball/controller/BaseballGameController.java
@@ -1,20 +1,17 @@
 package baseball.controller;
 
-import static baseball.domain.NumberGenerator.createNonDuplicateNumbers;
-
-import baseball.domain.Referee;
+import baseball.service.BaseballService;
 import baseball.view.InputView;
 import baseball.view.OutputView;
-import java.util.List;
 
 public class BaseballGameController {
 
-    private final Referee referee;
+    BaseballService baseballService;
 
-    private String playResult;
+    String playResult;
 
     public BaseballGameController() {
-        referee = new Referee();
+        baseballService = new BaseballService();
         playResult = "";
     }
 
@@ -24,10 +21,8 @@ public class BaseballGameController {
     }
 
     private void processGame() {
-        List<Integer> answer = createNonDuplicateNumbers();
-
         while (!playResult.equals("0볼 3스트라이크")) {
-            playResult = referee.compare(answer, InputView.askNumbers());
+            playResult = baseballService.playGame(InputView.askNumbers());
             OutputView.displayPlayResult(playResult);
         }
     }
---
+++ src/main/java/baseball/service/BaseballService.java
@@ -0,0 +1,22 @@
+package baseball.service;
+
+import static baseball.domain.NumberGenerator.createNonDuplicateNumbers;
+
+import baseball.domain.Referee;
+import java.util.List;
+
+public class BaseballService {
+
+    private final Referee referee;
+
+    private List<Integer> answer;
+
+    public BaseballService() {
+        this.referee = new Referee();
+        this.answer = createNonDuplicateNumbers();
+    }
+
+    public String playGame(final List<Integer> player) {
+        return referee.compare(answer, player);
+    }
+}
